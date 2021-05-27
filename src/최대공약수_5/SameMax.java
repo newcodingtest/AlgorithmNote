a13555fe69e3298c0c8d940df4d442d7f0b92336
윤주영 <pulpul8282@naver.com>
2021-05-27 22:13:36 +0900

    두수의 최대공약수 구하기

---
+++ src/최대공약수_5/SameMax.java
@@ -0,0 +1,20 @@
+package 최대공약수_5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SameMax {
+    public static void main(String[] args){
+        int x=12;
+        int y=18;
+
+        int max=1;
+
+        for(int i=1; i<=18; i++){
+            if(x%i == 0 && y%i == 0){
+             max=i;
+            }
+        }
+        System.out.println("max = " + max);
+    }
+}
