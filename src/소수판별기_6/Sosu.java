eaa4c097a8f75e0eeca94019f61f65f0ba57e456
윤주영 <pulpul8282@naver.com>
2021-05-27 22:54:14 +0900

    입력받은 숫자 소수 판별하기

---
+++ src/소수판별기_6/Sosu.java
@@ -0,0 +1,23 @@
+package 소수판별기_6;
+
+import java.util.Scanner;
+
+public class Sosu {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int x =sc.nextInt();
+
+        int cnt=0;
+        for(int i=1; i<=x; i++){
+            if(x%i==0){
+                cnt++;
+            }
+        }
+        if(cnt>2){
+            System.out.println(" 해당 숫자는 소수가 아닙니다.");
+            System.out.println("cnt = " + cnt);
+        }else{
+            System.out.println(" 해당 숫자는 소수입니다.");
+        }
+    }
+}
