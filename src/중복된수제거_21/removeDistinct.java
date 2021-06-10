db1e334cb7fc00ac87853cb088560ae4084332e7
윤주영 <pulpul8282@naver.com>
2021-06-10 20:29:33 +0900

    21번_입력된 값 중에 중복 값 제거

--- src/중복된수제거_21/removeDistinct.java
+++ src/중복된수제거_21/removeDistinct.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 //중복된 수 제거
-//방법 1. 배열 2. 해쉬셋 3.링크드해쉬셋 4.트리셋
+//사용할 수 있는 방법 1. 배열 2. 해쉬셋 3.링크드해쉬셋 4.트리셋
 public class removeDistinct {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
@@ -17,7 +17,10 @@ public class removeDistinct {
         //입력받은수 공백으로 구분해주기
         String[] arr1=arr.split(" ");
 
-        //해쉬셋은 중복을 알아서 제거해준다.
+        ArrayList list = new ArrayList();
+
+//첫번째 방법 해쉬셋
+/*        //해쉬셋은 중복을 알아서 제거해준다.
         Set<String> set = new HashSet<>();
 
         for (String item : arr1){
@@ -25,8 +28,21 @@ public class removeDistinct {
         }
 
         Stream<String> set1=set.stream();
-        set1.forEach(test-> System.out.println("test = " + test));
-      
+        set1.forEach(test-> System.out.println("test = " + test));*/
+
+//두번째 방법 배열
+        for(String test: arr1){
+            if(!list.contains(test)) {
+                list.add(test);
+            }
+        }
+
+        for (int i = 0; i <list.size() ; i++) {
+            System.out.println(list.get(i));
+        }
+
+
+
 
 
 
