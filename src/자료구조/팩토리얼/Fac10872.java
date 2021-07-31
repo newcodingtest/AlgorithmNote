0dcb6b38dce5095d87019d9bba1fef6836893c7d
윤주영 <pulpul8282@naver.com>
2021-07-31 23:08:14 +0900

    팩토리얼_10872

---
+++ src/자료구조/팩토리얼/Fac10872.java
@@ -0,0 +1,45 @@
+package 자료구조.팩토리얼;
+/*
+문제:
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력:
+첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+
+출력:
+첫째 줄에 N!을 출력한다.
+
+예제 입력:
+10
+0
+예제 출력:
+3628800
+1
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Fac10872 {
+    public static int recall(int n){
+        if(n<=0) {
+            return 1;
+        }else {
+            return n * recall(n - 1);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb =new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        sb.append(recall(input));
+
+        System.out.println(sb);
+
+    }
+}
