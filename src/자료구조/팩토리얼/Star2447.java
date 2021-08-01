14699fbdfe9de86bcddd1cda57f718c4a5bcd197
윤주영 <pulpul8282@naver.com>
2021-08-01 22:01:03 +0900

    재귀_백준2447

---
+++ src/자료구조/팩토리얼/Star2447.java
@@ -0,0 +1,96 @@
+package 자료구조.팩토리얼;
+
+import java.io.*;
+import java.util.Scanner;
+
+/*문제
+        재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
+
+        크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
+
+        ***
+        * *
+        ***
+        N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 예를 들어 크기 27의 패턴은 예제 출력 1과 같다.
+
+        입력
+        첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3k이며, 이때 1 ≤ k < 8이다.
+
+        출력
+        첫째 줄부터 N번째 줄까지 별을 출력한다.
+
+        예제 입력
+        27
+        예제 출력
+        ***************************
+        * ** ** ** ** ** ** ** ** *
+        ***************************
+        ***   ******   ******   ***
+        * *   * ** *   * ** *   * *
+        ***   ******   ******   ***
+        ***************************
+        * ** ** ** ** ** ** ** ** *
+        ***************************
+        *********         *********
+        * ** ** *         * ** ** *
+        *********         *********
+        ***   ***         ***   ***
+        * *   * *         * *   * *
+        ***   ***         ***   ***
+        *********         *********
+        * ** ** *         * ** ** *
+        *********         *********
+        ***************************
+        * ** ** ** ** ** ** ** ** *
+        ***************************
+        ***   ******   ******   ***
+        * *   * ** *   * ** *   * *
+        ***   ******   ******   ***
+        ***************************
+        * ** ** ** ** ** ** ** ** *
+        ****************************/
+public class Star2447 {
+    static boolean[][] map;
+    private static void divide(int row, int col, int len)throws IOException{
+        //분할 정복으로 생각해라
+        //3의 제곱으로 패턴이 생성된다.
+        if(len == 1){
+            map[row][col] = true;
+        }else{
+            int newlen = len/3;
+            for (int i = 0; i <3 ; i++) {
+                for (int j = 0; j <3 ; j++) {
+                    if(i == 1 && j ==1){
+                        map[i][j]=false;
+                    }else{
+                        divide(row+i*newlen,col+j*newlen,newlen);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        map = new boolean[input][input];
+
+        divide(0,0,input);
+
+        for (int i = 0; i <input ; i++) {
+            for (int j = 0; j <input ; j++) {
+                if(map[i][j]){
+                    bw.write("*");
+                }else{
+                    bw.write(" ");
+                }
+            }
+            bw.newLine();
+        }
+            bw.flush();
+            bw.close();
+    }
+}
